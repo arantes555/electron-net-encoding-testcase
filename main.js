1dc569bec4184b431913c759fa8aefabdc016cd9
mehdi <mehdi@stash.pm>
2017-03-11 18:31:17 +0100

    Actually, the second one was catchable. Update to demonstrate that (and also prove the first one is not)

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const {app, net} = require('electron')
+const { app, net } = require('electron')
 const TestServer = require('./server')
 
 const local = new TestServer()
 const base = `http://${local.hostname}:${local.port}`
 
-const doRequest = (url) => new Promise(resolve => {
+const doRequest = (url) => new Promise((resolve) => {
   const request = net.request(url)
   request.on('response', (response) => {
     console.log(`STATUS: ${response.statusCode}`)
@@ -15,8 +15,17 @@ const doRequest = (url) => new Promise(resolve => {
       console.log(`BODY: ${chunk}`)
     })
     response.on('end', () => {
+      console.log()
       resolve()
     })
+    response.on('error', err => {
+      console.error('ResponseError!', err, '\n')
+      resolve()
+    })
+  })
+  request.on('error', err => {
+    console.error('RequestError!', err, '\n')
+    resolve()
   })
   request.end()
 })
@@ -25,15 +34,22 @@ app.on('ready', async () => {
   await local.start()
   console.log('Server started')
 
-  await doRequest(`${base}/hello`)
+  try { // should be useless, my promise doesn't know how to reject
+    await doRequest(`${base}/hello`)
 
-  await doRequest(`${base}/gzip`)
+    await doRequest(`${base}/gzip`)
 
-  // Demonstration of https://github.com/electron/electron/issues/8867
-  await doRequest(`${base}/sdch`)
+    // Demonstration of https://github.com/electron/electron/issues/8867#issuecomment-285306575
+    // EDIT: this one is actually catchable
+    await doRequest(`${base}/invalid-content-encoding`)
 
-  // Demonstration of https://github.com/electron/electron/issues/8867#issuecomment-285306575
-  // await doRequest(`${base}/invalid-content-encoding`)
+
+    console.log('GOING TO FAIL\n')
+    // Demonstration of https://github.com/electron/electron/issues/8867
+    await doRequest(`${base}/sdch`)
+  } catch (err) {
+    console.error('An Error happened:', err)
+  }
 
   await local.stop()
   app.quit()
